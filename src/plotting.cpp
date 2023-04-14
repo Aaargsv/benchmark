7258c135de1bfa5c3db878bb3f64239f2cd6c7d9
Alexey <vl5znd@gmail.com>
2023-04-14 12:15:54 +0300

    +plotting

---
+++ src/plotting.cpp
@@ -0,0 +1,45 @@
+#include "plotting.h"
+#include "stat.h"
+#include "trase.hpp"
+#include <fstream>
+#include <vector>
+#include <string>
+
+int create_plot(std::vector<Mesurement> &ms, const std::string &plot_name, 
+                const std::string &x_label, const std::string &y_label) 
+{
+         /// create figure and axis
+    auto fig = trase::figure();
+    auto ax = fig->axis();
+
+    /// create x and y vectors and set y = sin(x)
+    int n = ms.size(); 
+    std::vector<int> x(n);
+    std::vector<double> y(n);
+    for (int i = 0; i < n; ++i) {
+        x[i] = ms[i].data_size;
+        y[i] = ms[i].avr_duration;
+    }
+
+    /// create a trase dataset and then plot it using a line geometry
+    auto data = trase::create_data().x(x).y(y);
+    auto plt = ax->line(data);
+
+    /// label axis
+    ax->xlabel(x_label.c_str());
+    ax->ylabel(y_label.c_str());
+
+    // output to chosen backend
+    std::string file_name = plot_name + ".svg";
+    std::ofstream out(file_name);
+    if (!out) {
+        std::cerr << "[Error]: can't create plot file" << std::endl;
+        return 1;
+    }
+
+    trase::BackendSVG backend(out);
+    fig->draw(backend);
+    out.close();
+
+    return 0;
+}
--- src/tests.cpp
+++ src/tests.cpp
@@ -1,16 +1,16 @@
 #include "tests.h"
 #include "sockets.h"
+#include "stat.h"
+#include "plotting.h"
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <iostream>
 #include <random>
 #include <ctime>
 #include <chrono>
+#include <vector>
+#include <cmath>
 
-struct Mesurement {
-    std::chrono::duration<float> avr_duration;
-    int data_size;
-};
 
 void generate_random_byte_array(char *buf, int len)
 {
@@ -20,23 +20,100 @@ void generate_random_byte_array(char *buf, int len)
     }
 }
 
-int ping(SOCKET server_socket, int start_data_size,
-         int power, int repetitions)
+int ping(SOCKET server_socket, int start_data_size, int end_data_size,
+          std::vector<Mesurement> &mesurements)
 {
+    std::vector<char> buffer(end_data_size);
+ 
+    int data_size;
+    for (data_size = start_data_size; data_size <= end_data_size; data_size++) {
+        buffer[0] = PINGING;
+        std::vector<double> durations;
+        int rep = 1000;
+        while (true) {
+            generate_random_byte_array(buffer.data() + 1, data_size - 1);
+            auto t1 = std::chrono::high_resolution_clock::now();
 
+
+            int error_code = send(server_socket, buffer.data(), data_size, 0);
+            if (error_code == SOCKET_ERROR) {
+                std::cout << "[Error]: can't send" << std::endl;
+                std::cout << WSAGetLastError() << std::endl;
+                return 1;
+            }
+
+            error_code = recv(server_socket, buffer.data(), data_size, 0);
+            if (error_code == SOCKET_ERROR || error_code == 0) {
+                std::cout << "[Error]: can't receive" << std::endl;
+                std::cout << WSAGetLastError() << std::endl;
+                return 1;
+            }
+
+            auto t2 = std::chrono::high_resolution_clock::now();
+
+            durations.push_back(std::chrono::duration<double>(t2 - t1).count() / 2);
+            double avr =  average(durations, durations.size());
+            double sigma = standard_deviation(durations, durations.size());
+            if (sigma <= THRESHOLD * avr)
+                break;   
+        }
+
+        Mesurement ms;
+        ms.avr_duration = average(durations, durations.size());
+        ms.data_size = data_size;
+        mesurements.push_back(ms);
+        buffer[0] = INCREMENT_DATA_SIZE;
+        send(server_socket, buffer.data(), data_size, 0);
+    }
+
+    buffer[0] = STOP_PING;
+    send(server_socket, buffer.data(), data_size, 0);
+    return 0;
 }
 
-int pong(SOCKET server_socket, int start_data_size,
-         int power, int repetitions)
+int pong(SOCKET client_socket, int start_data_size, int end_data_size)
 {
+    std::vector<char> buffer(end_data_size);
+    int data_size = start_data_size;
+    while (true) {
+        int error_code = recv(client_socket, buffer.data(), data_size, 0);
+        if (error_code == SOCKET_ERROR || error_code == 0) {
+            std::cerr << "[Error]: can't receive" << std::endl;
+            std::cout << WSAGetLastError() << std::endl;
+            return 1;
+        }
+        if (buffer[0] ==  STOP_PING)
+            return 0;
 
+        if (buffer[0] == INCREMENT_DATA_SIZE) {
+            data_size++;
+            continue;
+        }
+            
+        error_code = send(client_socket, buffer.data(), data_size, 0);
+        if (error_code == SOCKET_ERROR) {
+            std::cout << "[Error]: can't send" << std::endl;
+            std::cout << WSAGetLastError() << std::endl;
+            return 1;
+        }
+
+    }
 }
 
-int test_two_nodes(bool is_server, char *server_IP, int port)
+int test_two_nodes(bool is_server, char *server_IP, int port, int start_data_size, int end_data_size)
 {
+
+    if (start_data_size >= end_data_size) {
+        std::cout << "[Error]: start_data_size >= end_data_size" << std::endl;
+        return 1;
+    }
+
     SOCKET server_socket;
     if (is_server) {
-        open_server_connection(server_socket, server_IP, port);
+        if(open_server_connection(server_socket, server_IP, port, SOCK_STREAM)) {
+            std::cerr << "[Error]: can't open server connection" << std::endl;
+            return 1;
+        }
         sockaddr_in client_info;
         int client_info_size = sizeof(client_info);
         memset(&client_info, 0, sizeof(client_info));
@@ -56,9 +133,28 @@ int test_two_nodes(bool is_server, char *server_IP, int port)
             std::cout << "Client connected with IP address " << client_IP << std::endl;
         }
 
+        if (pong(client_socket, start_data_size, end_data_size)) {
+            std::cerr << "[Error]: can't pong" << std::endl;
+            return 1;
+        } 
+
     } else {
-        establish_connection_with_node(server_socket, server_IP, port);
+        if(establish_connection_with_node(server_socket, server_IP, port, SOCK_STREAM)) {
+            std::cerr << "[Error]: can't establish connection with node" << std::endl;
+            return 1;
+        }
+        std::vector<Mesurement> mesurements;
+        if (ping(server_socket, start_data_size, end_data_size, mesurements)) {
+            std::cerr << "[Error]: can't ping" << std::endl;
+            return 1;
+        }
+        
+        if (create_plot(mesurements, "ping-pong", "packet size", "delay")) {
+            std::cerr << "[Error]: can't plot" << std::endl;
+            return 1;
+        }
     }
+    return 0;
 }
 
 
